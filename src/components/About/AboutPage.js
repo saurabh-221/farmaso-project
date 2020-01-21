5bc2965f2e31b974276690d481f9de509a2ddafe
Saurabh Jain <saurabhjain291197@gmail.com>
2020-01-22 05:03:43 +0530

    a lot of changes done in ui like products and about-us pages added

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -1,9 +1,89 @@
-import React from 'react'
 
-export default function AboutPage() {
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBMask, MDBRow, MDBCol,  MDBBtn, MDBView, MDBContainer, MDBFormInline } from "mdbreact";
+
+class AboutPage extends Component {
+  state = {
+    collapsed: false
+  };
+
+  handleTogglerClick = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  };
+
+  render() {
+    const navStyle = { marginTop: "4rem" };
+    const overlay = (
+      <div
+        id="sidenav-overlay"
+        style={{ backgroundColor: "transparent" }}
+        onClick={this.handleTogglerClick}
+      />
+    );
     return (
-        <div>
-            AboutPage
-        </div>
-    )
+      <>
+        <Router>
+          <div>
+            
+            {this.state.collapsed && overlay}
+          </div>
+        </Router>
+        <MDBView src={`https://mdbootstrap.com/img/Photos/Others/images/76.jpg`} fixed>
+          <MDBMask className="rgba-white-light d-flex justify-content-center align-items-center">
+            <MDBContainer>
+              <MDBRow>
+                <MDBCol md="12" className="mb-4 white-text text-center">
+                  <h1 class="display-3 mb-0 pt-md-5 pt-5 white-text font-weight-bold" ><span className="indigo-text font-weight-bold">SAURABH</span> AND <span className="indigo-text font-weight-bold">VISHNU</span></h1>
+                  <hr className="hr-light my-4" />
+                  <h5 className="text-uppercase pt-md-5 pt-sm-2 pt-5 pb-md-5 pb-sm-3 pb-5 white-text font-weight-bold">Two bad-boys in the silicon valley</h5>
+                 
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </MDBMask>
+        </MDBView>
+        <main>
+          <MDBContainer>
+            <MDBRow className="py-5">
+              <MDBCol md="12" className="text-center">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                  qui officia deserunt mollit anim id est laborum.</p>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                  totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
+                  sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                  consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+                  dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
+                  tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,
+                  quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                  consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+                  molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                  qui officia deserunt mollit anim id est laborum.</p>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                  totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
+                  sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                  consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+                  dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
+                  tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,
+                  quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                  consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
+                  molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        </main>
+      </>
+    );
+  }
 }
+
+export default AboutPage;
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { MDBMask, MDBRow, MDBCol, MDBBtn, MDBView, MDBContainer, MDBCard, MDBCardBody, MDBIcon } from "mdbreact";
+
 import './HomePage.css';
 
 class HomePage extends Component {
@@ -128,6 +129,95 @@ https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg
             </MDBRow>
           </MDBCardBody>
         </MDBCard>
+
+
+        <section className="text-center my-5">
+      <h2 className="h1-responsive font-weight-bold text-center my-5">
+        Our bestsellers
+      </h2>
+      <p className="grey-text text-center w-responsive mx-auto mb-5">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+        error amet numquam iure provident voluptate esse quasi, veritatis
+        totam voluptas nostrum quisquam eum porro a pariatur veniam.
+      </p>
+      <MDBRow>
+        <MDBCol lg="3" md="6" className="mb-lg-0 mb-4">
+          <MDBCard collection className="z-depth-1-half">
+            <div className="view zoom">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/5.jpg"
+                className="img-fluid"
+                alt=""
+              />
+              <div className="stripe dark">
+                <a href="#!">
+                  <p>
+                    Red trousers <MDBIcon icon="angle-right" />
+                  </p>
+                </a>
+              </div>
+            </div>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol lg="3" md="6" className="mb-lg-0 mb-4">
+          <MDBCard collection className="z-depth-1-half">
+            <div className="view zoom">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/8.jpg"
+                className="img-fluid"
+                alt=""
+              />
+              <div className="stripe dark">
+                <a href="#!">
+                  <p>
+                    Sweatshirt <MDBIcon icon="angle-right" />
+                  </p>
+                </a>
+              </div>
+            </div>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol lg="3" md="6" className="mb-lg-0 mb-4">
+          <MDBCard collection className="z-depth-1-half">
+            <div className="view zoom">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/9.jpg"
+                className="img-fluid"
+                alt=""
+              />
+              <div className="stripe dark">
+                <a href="#!">
+                  <p>
+                    Accessories <MDBIcon icon="angle-right" />
+                  </p>
+                </a>
+              </div>
+            </div>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol lg="3" md="6" className="mb-lg-0 mb-4">
+          <MDBCard collection className="z-depth-1-half">
+            <div className="view zoom">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/7.jpg"
+                className="img-fluid"
+                alt=""
+              />
+              <div className="stripe dark">
+                <a href="#!">
+                  <p>
+                    Sweatshirt <MDBIcon icon="angle-right" />
+                  </p>
+                </a>
+              </div>
+            </div>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+
+    
+    </section>
+
       </div>
     );
   }
